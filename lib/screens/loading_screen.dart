ea7490d65bd52947ea558a2865cb51d5394389cc
CincoCoding <coderdavidgarcia@gmail.com>
2023-05-09 22:18:27 -0500

    Getting Actual Weather Data From OpenWeatherMap API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,49 +1,36 @@
 import 'package:flutter/material.dart';
+import '../services/networking.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
+const apiKey = "baa80af84153ba4e0d3e760d96f202af";
+double latitude = 0;
+double longitude = 0;
+
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
-  }
-
-  void getData() async {
-    Response response = await get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=baa80af84153ba4e0d3e760d96f202af'));
-    if (response.statusCode == 200) {
-      String data = response.body;
-      Map decodedData = jsonDecode(data);
-
-      double latitude = decodedData['coord']['lat'];
-      double longitude = decodedData['coord']['lon'];
-      double temperature = decodedData['main']['temp'];
-      int condition = decodedData['weather'][0]['id'];
-      String city = decodedData['name'];
-
-      print(latitude);
-      print(longitude);
-      print(temperature);
-      print(condition);
-      print(city);
-    } else {
-      print(response.statusCode);
-    }
-  }
-
   void initState() {
     // TODO: implement initState
     super.initState();
-    getLocation();
-    getData();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    Location location = Location();
+
+    await location.getCurrentLocation();
+
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
+    print(weatherData);
   }
 
   @override
---
+++ lib/services/networking.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper {
+  NetworkHelper(this.url);
+
+  final String url;
+
+  Future getData() async {
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+
+      Map decodedData = jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
+
+//
+// print(latitude);
+// print(longitude);
+// print(temperature);
+// print(condition);
+// print(city);
